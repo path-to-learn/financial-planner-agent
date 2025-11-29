83321d9a9a0a2af2f89f5fc8b9eec72fb37c438d
Prashant Dathwal <prashantd@Prashants-MacBook-Pro.local>
2025-11-28 22:08:46 -0800

    Add .DS_Store to gitignore

---
+++ java-finance-service/src/main/java/com/pathtolearn/financial/java_finance_service/JavaFinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pathtolearn.financial.java_finance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFinanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFinanceServiceApplication.class, args);
+	}
+
+}
---
+++ java-finance-service/src/test/java/com/pathtolearn/financial/java_finance_service/JavaFinanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathtolearn.financial.java_finance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFinanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
